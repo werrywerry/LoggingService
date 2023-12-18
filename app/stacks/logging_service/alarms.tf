73c7e0645f78b20eebbdef17a9c13a15cba6fbf3
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-12-18 14:22:33 +1100

    Add alarms

---
+++ app/stacks/logging_service/alarms.tf
@@ -0,0 +1,77 @@
+module "cloudwatch_alarms" {
+  source = "git::https://bitbucket.org/nsw-education/terraform-module-cloudwatch-alarms.git?ref=release/1.0.1"
+
+  env = "Dev"
+
+  service_name = "LoggingService"
+
+  resource_list = {
+    "lambdas" : [
+      {
+        "lambda" : "LoggingService-SubscriptionFilterHandler-${var.env}",
+        "timeout" : 900,
+        "concurrency" : 1,
+        "memory" : 128
+      },
+      {
+        "lambda" : "LoggingService-SplunkForwarder-${var.env}",
+        "timeout" : 3,
+        "concurrency" : 10,
+        "memory" : 128
+      }
+    ],
+    "rdss" : [
+    ],
+    "apis" : [
+    ],
+    "dynamos" : [
+    ],
+    "eventbridges" : [
+      {
+        "name" : "default",
+        "ruleName" : "LoggingService-NewLogGroupCreated-Dev"
+      }
+    ],
+    "queues" : [
+    ],
+    "sns_subscriptions" : [
+    ]
+  }
+
+    eventbridge_thresholds = {
+    "default" = {
+
+    }
+  }
+
+  lambda_thresholds = {
+    "LoggingService-SubscriptionFilterHandler-Dev" = {
+      success_rate_threshold            = null
+      errors_threshold                  = null
+      duration_threshold                = 120
+      memory_underutilization_threshold = null
+      memory_overutilization_threshold  = null
+      concurrent_executions_threshold   = null
+      throttles_threshold               = null
+    },
+    "LoggingService-SubscriptionFilterHandler-Dev" = {
+      success_rate_threshold            = null
+      errors_threshold                  = null
+      duration_threshold                = 2
+      memory_underutilization_threshold = null
+      memory_overutilization_threshold  = null
+      concurrent_executions_threshold   = null
+      throttles_threshold               = null
+    }
+  }
+}
+
+module "cloudwatch_dashboards" {
+  source = "git::https://bitbucket.org/nsw-education/terraform-module-cloudwatch-dashboards.git?ref=release/1.0.0"
+
+  env = module.cloudwatch_alarms.env
+
+  service_name = module.cloudwatch_alarms.service_name
+
+  resource_list = module.cloudwatch_alarms.resource_list
+}
